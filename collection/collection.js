1aab6cf3698d931aaa4631eacd00fa09753bad33
Borelset <xdnzxy@gmail.com>
2016-11-03 02:20:08 +0800

    Add files via upload

---
+++ collection/collection.js
@@ -0,0 +1,7 @@
+expert.user = new Mongo.Collection("user");
+
+expert.info = new Mongo.Collection("info");
+
+expert.course = new Mongo.Collection("course");
+
+expert.student = new Mongo.Collection("student");
