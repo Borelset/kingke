21f2a90ec6a3fdfa91ab9c054abdc1936739be79
Borelset <xdnzxy@gmail.com>
2016-11-03 23:56:11 +0800

    add

--- server/methods/qr.js
+++ server/methods/qr.js
@@ -23,7 +23,7 @@ exports.createqr = function(cid) {
     post_cont = JSON.stringify(post_cont);
     var post_result = HTTP.post(ticket_gen_url, {content : post_cont});
     var result_obj = JSON.parse(post_result.content);
-    var qrcodeImg = 'https://mp.weixin.qq.com/cgi-bin/showqrcode?ticket=' + encodeURIComponent(qrcodeJson.ticket);
+    var qrcodeImg = 'https://mp.weixin.qq.com/cgi-bin/showqrcode?ticket=' + encodeURIComponent(result_obj.ticket);
     var qrc_rel = {};
     qrc_rel.cid = cid;
     qrc_rel.url = qrcodeImg;
