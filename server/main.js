921f91fe9a4e0b1e3ac4012002c5926b0fddf5b9
Borelset <xdnzxy@gmail.com>
2016-11-03 10:31:42 +0800

    Update main.js

--- server/main.js
+++ server/main.js
@@ -37,7 +37,7 @@ Meteor.startup(() => {
       var token_result = HTTP.get(token_url);
       var access_token = token_result.data.access_token;
       var menu_url = "https://api.weixin.qq.com/cgi-bin/menu/create?access_token=" + access_token;
-      var menu_data = '{"button":[{"type":"view", "name" : "查看个人信息","url" : "https://open.weixin.qq.com/connect/oauth2/authorize?appid=' + config.appID + '&redirect_uri=http%3A%2F%2F' + config.url + '%2Finfo&response_type=code&scope=snsapi_userinfo&state=lc#wechat_redirect"},{"name" : "课程","sub_button" :[{"type":"view","name" : "所有课程","url" :"",},{"type":"view","name" : "我的课程","url" : "",}]}]}';
+      var menu_data = '{"button":[{"type":"view", "name" : "查看个人信息","url" : "https://open.weixin.qq.com/connect/oauth2/authorize?appid=' + config.appID + '&redirect_uri=http%3A%2F%2F' + config.url + '%2Finfo&response_type=code&scope=snsapi_userinfo&state=lc#wechat_redirect"},{"name" : "课程","sub_button" :[{"type":"view","name" : "所有课程","url" :""},{"type":"view","name" : "我的课程","url" : ""}]}]}';
       var menu_result = HTTP.post(menu_url, {content: menu_data});
       res.end("set success" + menu_result.content);
     } catch (err) {
