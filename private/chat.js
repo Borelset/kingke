e52c6f9412c8d3be2530692d4032db53394eb8a8
Borelset <xdnzxy@gmail.com>
2016-11-09 21:21:05 +0800

    add

---
+++ private/chat.js
@@ -0,0 +1,17 @@
+var collection = require("../collection/collection.js");
+var Messages = collection.message;
+var Chatter = collection.chatter;
+
+sendMessage = function(fname) {
+  var name = fname;
+  var message = document.getElementById('message');
+  if (message.value !== '') {
+    Messages.insert({
+      name: name,
+      message: message.value,
+      time: Date.now(),
+    });
+    document.getElementById('message').value = '';
+    message.value = '';
+  }
+};
