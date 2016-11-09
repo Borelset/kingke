4d8ef1cbfa47c91febdca049fc9b8ee05e46b6e9
Borelset <xdnzxy@gmail.com>
2016-11-09 22:54:18 +0800

    add

--- server/main.js
+++ server/main.js
@@ -1,6 +1,5 @@
 import { Meteor } from 'meteor/meteor';
 import { HTTP } from 'meteor/http';
-import './main.html';
 var config = require("./config.js");
 var collection = require("../collection/collection.js");
 var getinfoJS = require("./methods/getinfo.js");
@@ -307,37 +306,4 @@ Meteor.startup(() => {
     
   }, {where: 'server'});
 
-Router.route('/chat', function() {
-  this.render('Chat');
-});
-
-Template.Chat.helpers({
-  messages: function() {
-    return Messages.find({}, { sort: { time: -1 } });
-  }
-});
-
-sendMessage = function() {
-  var name = '陌生人';
-  var message = document.getElementById('message');
-  if (message.value !== '') {
-    Messages.insert({
-      name: name,
-      message: message.value,
-      time: Date.now(),
-    });
-
-    document.getElementById('message').value = '';
-    message.value = '';
-  }
-};
-
-Template.Chat.events = {
-  'keydown input#message': function(event) {
-    if (event.which === 13) {
-      sendMessage();
-    }
-  }
-};
-  
 });
